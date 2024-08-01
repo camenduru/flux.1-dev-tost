bce70db5caa34d714c646e621ee8c607e60e0641
camenduru <camenduru@gmail.com>
2024-08-01 19:35:36 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+🐣 Please follow me for new updates https://twitter.com/camenduru <br />
+🔥 Please join our discord server https://discord.gg/k5BwmmvJJU <br />
+🥳 Please join my patreon community https://patreon.com/camenduru <br />
+
+###  🥪 Tost
+https://tost.ai
+
+### 🧬 Code
+https://github.com/black-forest-labs/flux
+
+### 🌐 Page
+https://blackforestlabs.ai/
+
+### 🖼 Output
+
+
+### 🏢 Sponsor
+https://runpod.io
---
+++ worker_runpod.py
@@ -0,0 +1,96 @@
+import os, json, requests, runpod
+
+import random
+import torch
+import numpy as np
+from PIL import Image
+import nodes
+from nodes import NODE_CLASS_MAPPINGS
+from comfy_extras import nodes_custom_sampler
+from comfy import model_management
+
+discord_token = os.getenv('com_camenduru_discord_token')
+web_uri = os.getenv('com_camenduru_web_uri')
+web_token = os.getenv('com_camenduru_web_token')
+
+DualCLIPLoader = NODE_CLASS_MAPPINGS["DualCLIPLoader"]()
+UNETLoader = NODE_CLASS_MAPPINGS["UNETLoader"]()
+RandomNoise = nodes_custom_sampler.NODE_CLASS_MAPPINGS["RandomNoise"]()
+BasicGuider = nodes_custom_sampler.NODE_CLASS_MAPPINGS["BasicGuider"]()
+KSamplerSelect = nodes_custom_sampler.NODE_CLASS_MAPPINGS["KSamplerSelect"]()
+BasicScheduler = nodes_custom_sampler.NODE_CLASS_MAPPINGS["BasicScheduler"]()
+SamplerCustomAdvanced = nodes_custom_sampler.NODE_CLASS_MAPPINGS["SamplerCustomAdvanced"]()
+VAELoader = NODE_CLASS_MAPPINGS["VAELoader"]()
+VAEDecode = NODE_CLASS_MAPPINGS["VAEDecode"]()
+EmptyLatentImage = NODE_CLASS_MAPPINGS["EmptyLatentImage"]()
+
+with torch.inference_mode():
+    clip = DualCLIPLoader.load_clip("t5xxl_fp16.safetensors", "clip_l.safetensors", "flux")[0]
+    unet = UNETLoader.load_unet("flux1-dev.sft", "default")[0]
+    vae = VAELoader.load_vae("ae.sft")[0]
+
+@torch.inference_mode()
+def generate(input):
+    values = input["input"]
+
+    positive_prompt = values['positive_prompt']
+    width = values['width']
+    height = values['height']
+    seed = values['seed']
+    steps = values['steps']
+    sampler_name = values['sampler_name']
+    scheduler = values['scheduler']
+
+    if seed == 0:
+        seed = random.randint(0, 18446744073709551615)
+    print(seed)
+
+    cond, pooled = clip.encode_from_tokens(clip.tokenize(positive_prompt), return_pooled=True)
+    cond = [[cond, {"pooled_output": pooled}]]
+    noise = RandomNoise.get_noise(seed)[0] 
+    guider = BasicGuider.get_guider(unet, cond)[0]
+    sampler = KSamplerSelect.get_sampler(sampler_name)[0]
+    sigmas = BasicScheduler.get_sigmas(unet, scheduler, steps, 1.0)[0]
+    latent_image = EmptyLatentImage.generate(width, height)[0]
+    sample, sample_denoised = SamplerCustomAdvanced.sample(noise, guider, sampler, sigmas, latent_image)
+    model_management.soft_empty_cache()
+    decoded = VAEDecode.decode(vae, sample)[0].detach()
+    Image.fromarray(np.array(decoded*255, dtype=np.uint8)[0]).save("/content/flux.png")
+
+    result = "/content/flux.png"
+    response = None
+    try:
+        source_id = values['source_id']
+        del values['source_id']
+        source_channel = values['source_channel']     
+        del values['source_channel']
+        job_id = values['job_id']
+        del values['job_id']
+        default_filename = os.path.basename(result)
+        files = {default_filename: open(result, "rb").read()}
+        payload = {"content": f"{json.dumps(values)} <@{source_id}>"}
+        response = requests.post(
+            f"https://discord.com/api/v9/channels/{source_channel}/messages",
+            data=payload,
+            headers={"authorization": f"Bot {discord_token}"},
+            files=files
+        )
+        response.raise_for_status()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        if os.path.exists(result):
+            os.remove(result)
+
+    if response and response.status_code == 200:
+        try:
+            payload = {"jobId": job_id, "result": response.json()['attachments'][0]['url']}
+            requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        finally:
+            return {"result": response.json()['attachments'][0]['url']}
+    else:
+        return {"result": "ERROR"}
+
+runpod.serverless.start({"handler": generate})
